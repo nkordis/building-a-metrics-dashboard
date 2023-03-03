d71d2a6f73ff94d202910c6c7784a8474ccc6194
Nikos Kordis <nikoskordis@gmail.com>
2023-03-03 01:25:28 +0100

    Add jaeger tracing in backend

--- reference-app/backend/app.py
+++ reference-app/backend/app.py
@@ -1,9 +1,54 @@
 from flask import Flask, render_template, request, jsonify
+from prometheus_flask_exporter import PrometheusMetrics
+from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
 
 import pymongo
 from flask_pymongo import PyMongo
+from flask_cors import CORS
+
+from flask_opentracing import FlaskTracing
+from jaeger_client import Config
+from jaeger_client.metrics.prometheus import PrometheusMetricsFactory
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.instrumentation.wsgi import collect_request_attributes
 
 app = Flask(__name__)
+
+metrics = GunicornInternalPrometheusMetrics(app)
+CORS(app)
+
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
+
+metrics = PrometheusMetrics(app)
+# static information as metric
+metrics.info("app_info", "Application info", version="1.0.3")
+
+logging.getLogger("").handlers = []
+logging.basicConfig(format="%(message)s", level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+def init_tracer(service):
+
+    config = Config(
+        config={
+            "sampler": {"type": "const", "param": 1},
+            "logging": True,
+            "reporter_batch_size": 1,
+        },
+        service_name=service,
+        validate=True,
+        metrics_factory=PrometheusMetricsFactory(service_name_label=service),
+    )
+
+    # this call also sets opentracing.tracer
+    return config.initialize_tracer()
+
+
+tracer = init_tracer("trial")
+flask_tracer = FlaskTracing(tracer, True, app)
 
 app.config["MONGO_DBNAME"] = "example-mongodb"
 app.config[
@@ -15,7 +60,9 @@ mongo = PyMongo(app)
 
 @app.route("/")
 def homepage():
-    return "Hello World"
+with tracer.start_span('backend:homepage') as span:
+        span.log_kv({'event': 'homepage'})
+        return "Hello World"
 
 
 @app.route("/api")
@@ -33,6 +80,10 @@ def add_star():
     new_star = star.find_one({"_id": star_id})
     output = {"name": new_star["name"], "distance": new_star["distance"]}
     return jsonify({"result": output})
+
+@app.route("/metrics")
+def metrics():
+    return metrics.export()
 
 
 if __name__ == "__main__":
